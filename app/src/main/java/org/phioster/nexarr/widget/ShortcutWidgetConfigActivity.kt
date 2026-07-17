53a7374de3cfb38c005741f6748383257bd5de2a
Phioster <165709682+Phioster@users.noreply.github.com>
2026-07-17 20:27:57 +0200

    v0.63.5 fix: @OptIn ExperimentalLayoutApi for FlowRow in widget config

--- app/src/main/java/org/phioster/nexarr/widget/ShortcutWidgetConfigActivity.kt
+++ app/src/main/java/org/phioster/nexarr/widget/ShortcutWidgetConfigActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -72,6 +73,7 @@ class ShortcutWidgetConfigActivity : ComponentActivity() {
         setContent { ConfigScreen(appWidgetId) }
     }
 
+    @OptIn(ExperimentalLayoutApi::class)
     @Composable
     private fun ConfigScreen(appWidgetId: Int) {
         val ctx = LocalContext.current
